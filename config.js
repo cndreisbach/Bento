d89cdf989b91276fc9fd4ae35ef573aeeac7ddd5
Clinton Dreisbach <clinton.dreisbach@loom.com>
2022-02-24 09:34:33 -0500

    first update

--- config.js
+++ config.js
@@ -11,29 +11,29 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
+  name: "Clinton",
   imageBackground: false,
   openInNewTab: true,
   twelveHourFormat: false,
 
   // Greetings
-  greetingMorning: 'Good morning!',
-  greetingAfternoon: 'Good afternoon,',
-  greetingEvening: 'Good evening,',
-  greetingNight: 'Go to Sleep!',
+  greetingMorning: "Good morning!",
+  greetingAfternoon: "Good afternoon,",
+  greetingEvening: "Good evening,",
+  greetingNight: "Good evening,",
 
   // Layout
-  bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+  bentoLayout: "bento", // 'bento', 'lists', 'buttons'
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
-  weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
-  weatherUnit: 'C', // 'F', 'C'
-  language: 'en', // More languages in https://openweathermap.org/current#multi
+  weatherKey: "dcd19d748f9a4da76524de8ec2bac3f8", // Write here your API Key
+  weatherIcons: "Nord", // 'Onedark', 'Nord', 'Dark', 'White'
+  weatherUnit: "F", // 'F', 'C'
+  language: "en", // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  defaultLatitude: "36.074",
+  defaultLongitude: "-78.894",
 
   // Autochange
   autoChangeTheme: true,
@@ -43,8 +43,8 @@ const CONFIG = {
 
   // Autochange by hour options (24hrs format, string must be in: hh:mm)
   changeThemeByHour: false,
-  hourDarkThemeActive: '18:30',
-  hourDarkThemeInactive: '07:00',
+  hourDarkThemeActive: "18:30",
+  hourDarkThemeInactive: "07:00",
 
   // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
   // ├┴┐│ │ │  │ │ ││││└─┐
@@ -52,79 +52,79 @@ const CONFIG = {
 
   firstButtonsContainer: [
     {
-      id: '1',
-      name: 'Github',
-      icon: 'github',
-      link: 'https://github.com/',
+      id: "1",
+      name: "Hey",
+      icon: "mail",
+      link: "https://app.hey.com/",
     },
     {
-      id: '2',
-      name: 'Mail',
-      icon: 'mail',
-      link: 'https://mail.protonmail.com/',
+      id: "2",
+      name: "Todoist",
+      icon: "list-checks",
+      link: "https://todoist.com",
     },
     {
-      id: '3',
-      name: 'Todoist',
-      icon: 'trello',
-      link: 'https://todoist.com',
+      id: "3",
+      name: "YNAB",
+      icon: "banknote",
+      link: "https://app.youneedabudget.com/",
     },
     {
-      id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
+      id: "4",
+      name: "Calendar",
+      icon: "calendar",
+      link: "https://calendar.google.com/calendar/r",
     },
     {
-      id: '5',
-      name: 'Reddit',
-      icon: 'glasses',
-      link: 'https://reddit.com',
+      id: "5",
+      name: "Feedbin",
+      icon: "rss",
+      link: "https://feedbin.com/",
     },
     {
-      id: '6',
-      name: 'Odysee',
-      icon: 'youtube',
-      link: 'https://odysee.com/',
+      id: "6",
+      name: "NAS",
+      icon: "hard-drive",
+      link: "http://quickconnect.to/dreisbach",
     },
   ],
 
   secondButtonsContainer: [
     {
-      id: '1',
-      name: 'Music',
-      icon: 'headphones',
-      link: 'https://open.spotify.com',
+      id: "1",
+      name: "Music",
+      icon: "headphones",
+      link: "https://open.spotify.com",
     },
     {
-      id: '2',
-      name: 'twitter',
-      icon: 'twitter',
-      link: 'https://twitter.com/',
+      id: "2",
+      name: "twitter",
+      icon: "twitter",
+      link: "https://twitter.com/",
     },
     {
-      id: '3',
-      name: 'bot',
-      icon: 'bot',
-      link: 'https://discord.com/app',
+      id: "3",
+      name: "bot",
+      icon: "bot",
+      link: "https://discord.com/app",
     },
     {
-      id: '4',
-      name: 'Amazon',
-      icon: 'shopping-bag',
-      link: 'https://amazon.com/',
+      id: "4",
+      name: "Amazon",
+      icon: "shopping-bag",
+      link: "https://amazon.com/",
     },
     {
-      id: '5',
-      name: 'Hashnode',
-      icon: 'pen-tool',
-      link: 'https://hashnode.com/',
+      id: "5",
+      name: "Hashnode",
+      icon: "pen-tool",
+      link: "https://hashnode.com/",
     },
     {
-      id: '6',
-      name: 'Figma',
-      icon: 'figma',
-      link: 'https://figma.com/',
+      id: "6",
+      name: "Figma",
+      icon: "figma",
+      link: "https://figma.com/",
     },
   ],
 
@@ -135,46 +135,46 @@ const CONFIG = {
   // First Links Container
   firstlistsContainer: [
     {
-      icon: 'music',
-      id: '1',
+      icon: "music",
+      id: "1",
       links: [
         {
-          name: 'Inspirational',
-          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+          name: "Inspirational",
+          link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         },
         {
-          name: 'Classic',
-          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+          name: "Classic",
+          link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         },
         {
-          name: 'Oldies',
-          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+          name: "Oldies",
+          link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         },
         {
-          name: 'Rock',
-          link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+          name: "Rock",
+          link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
         },
       ],
     },
     {
-      icon: 'coffee',
-      id: '2',
+      icon: "coffee",
+      id: "2",
       links: [
         {
-          name: 'Linkedin',
-          link: 'https://www.linkedin.com',
+          name: "Linkedin",
+          link: "https://www.linkedin.com",
         },
         {
-          name: 'Dribbble',
-          link: 'https://www.dribbble.com',
+          name: "Dribbble",
+          link: "https://www.dribbble.com",
         },
         {
-          name: 'Trello',
-          link: 'https://www.trello.com',
+          name: "Trello",
+          link: "https://www.trello.com",
         },
         {
-          name: 'Slack',
-          link: 'https://www.slack.com',
+          name: "Slack",
+          link: "https://www.slack.com",
         },
       ],
     },
@@ -183,48 +183,48 @@ const CONFIG = {
   // Second Links Container
   secondListsContainer: [
     {
-      icon: 'binary',
-      id: '1',
+      icon: "binary",
+      id: "1",
       links: [
         {
-          name: 'Spotify',
-          link: 'https://www.spotify.com',
+          name: "Spotify",
+          link: "https://www.spotify.com",
         },
         {
-          name: 'Reddit',
-          link: 'https://www.reddit.com',
+          name: "Reddit",
+          link: "https://www.reddit.com",
         },
         {
-          name: 'Hashnode',
-          link: 'https://www.hashnode.com',
+          name: "Hashnode",
+          link: "https://www.hashnode.com",
         },
         {
-          name: 'Pocket',
-          link: 'https://www.pocket.com',
+          name: "Pocket",
+          link: "https://www.pocket.com",
         },
       ],
     },
     {
-      icon: 'github',
-      id: '2',
+      icon: "github",
+      id: "2",
       links: [
         {
-          name: 'Front',
-          link: 'https://www.reddit.com/r/Frontend/',
+          name: "Front",
+          link: "https://www.reddit.com/r/Frontend/",
         },
         {
-          name: 'Rust',
-          link: 'https://www.reddit.com/r/rust/',
+          name: "Rust",
+          link: "https://www.reddit.com/r/rust/",
         },
         {
-          name: 'Go',
-          link: 'https://www.reddit.com/r/golang/',
+          name: "Go",
+          link: "https://www.reddit.com/r/golang/",
         },
         {
-          name: 'Repos',
-          link: 'https://github.com/migueravila',
+          name: "Repos",
+          link: "https://github.com/migueravila",
         },
       ],
     },
   ],
-};
+}
